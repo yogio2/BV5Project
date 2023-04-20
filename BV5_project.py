ef17308b71403089df43266ebb7a817378430eca
yogio2 <44019560+yogio2@users.noreply.github.com>
2023-04-20 17:00:03 +0530

    Update BV5_project.py

--- BV5_project.py
+++ BV5_project.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+pip install dash-bootstrap-components
 import dash_bootstrap_components as dbc
 import pandas as pd
 
